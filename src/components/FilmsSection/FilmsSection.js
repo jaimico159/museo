b7ffb14a7775121187f2703f6bf55bc1aae9ff06
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-02-27 01:03:57 -0500

    updates film section

--- src/components/FilmsSection/FilmsSection.js
+++ src/components/FilmsSection/FilmsSection.js
@@ -1,5 +1,4 @@
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Image from "react-bootstrap/Image";
 import styled from "styled-components";
@@ -9,33 +8,43 @@ import { gallery_images } from "../../constants/constants";
 import React, { useState } from "react";
 import FilmCard from "./FilmCard";
 import AliceCarousel from "react-alice-carousel";
+import Slider from "react-slick";
+import SquareButton from "../../shared/SquareButton";
+import Title from "../../shared/Title";
 
 const filmsContainerStyles = {
   maxWidth: 1600,
+  width: "85%",
+};
+
+const mainContainer = {
+  width: "100%",
+  background: "rgb(248,247,245)",
+  maxWidth: 2600,
+  height: 700,
+  paddingTop: 50,
+  paddingBottom: 50,
+};
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
 };
 
 function FilmsSection() {
-  const items = [
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-    <FilmCard />,
-  ];
   return (
-    <Container style={filmsContainerStyles}>
-      <AliceCarousel
-        autoWidth={true}
-        disableButtonsControls={true}
-        items={items}
-      />
+    <Container style={mainContainer}>
+      <Container style={filmsContainerStyles}>
+        <Title text="Videos" />
+        <Slider {...settings}>
+          {gallery_images.map((url) => {
+            return <FilmCard src={url} />;
+          })}
+        </Slider>
+      </Container>
     </Container>
   );
 }
