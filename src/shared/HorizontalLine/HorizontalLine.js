2b9aa16959ea840ad7d0da7af5fb88b78cbaa858
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-03-04 22:03:05 -0500

    adds color to lines

--- src/shared/HorizontalLine/HorizontalLine.js
+++ src/shared/HorizontalLine/HorizontalLine.js
@@ -3,13 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: (props) => ({
-    background: "white",
+    background: props.color,
     width: props.width,
   }),
 });
 
-function HorizontalLine({ width }) {
-  const props = { width: width || 40 };
+function HorizontalLine({ width, color }) {
+  const props = { width: width || 40, color: color || "white" };
   const classes = useStyles(props);
   return <Divider classes={{ root: classes.root }} />;
 }
