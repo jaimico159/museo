485a74e5c8baf8b561af894cead4f169c40abb71
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-03-08 03:56:58 -0500

    adds font family to title

--- src/shared/Title/Title.js
+++ src/shared/Title/Title.js
@@ -1,12 +1,26 @@
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Title({ text, color }) {
+const useStyles = makeStyles({
+  root: (props) => ({
+    fontFamily: "MOD20",
+    letterSpacing: props.space,
+    marginBottom: props.marginBottom,
+  }),
+});
+
+function Title({ text, color, variant, space, marginBottom }) {
+  const props = {
+    space: space || 0,
+    marginBottom: marginBottom || 0,
+  };
+  const classes = useStyles(props);
+
   return (
     <Typography
-      variant="h4"
-      gutterBottom
-      style={{ marginBottom: 30 }}
+      variant={variant || "h4"}
       color={color}
+      classes={{ root: classes.root }}
     >
       {text}
     </Typography>
