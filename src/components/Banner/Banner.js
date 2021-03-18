d1e7f5ffaa2ec2f4cee91461bde65d842e8b15e2
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-03-17 22:19:20 -0500

    fix top nav

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,30 +1,29 @@
 import { Fragment } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import { urls } from "../../constants/constants";
+import { urls, banner_image_url } from "../../constants/constants";
 
 function Banner() {
   return (
     <Fragment>
       <Carousel interval={3000} controls={false}>
-        {urls.map(function (item, index) {
-          return (
-            <Carousel.Item key={index}>
-              <img
-                //className="d-block w-100"
-                src={item}
-                alt="First slide"
-                height="500px"
-                width="100%"
-              />
-              <Carousel.Caption>
+        {/* {urls.map(function (item, index) {
+          return ( */}
+        <Carousel.Item>
+          <img
+            //className="d-block w-100"
+            src={banner_image_url}
+            alt="First slide"
+            width="100%"
+          />
+          {/* <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>
                   Nulla vitae elit libero, a pharetra augue mollis interdum.
                 </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          );
-        })}
+              </Carousel.Caption> */}
+        </Carousel.Item>
+        {/* );
+        })} */}
       </Carousel>
     </Fragment>
   );
