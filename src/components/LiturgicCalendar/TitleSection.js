177728ce51d4e1ceb81167a21bf059338b93b0cb
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-03-19 20:39:24 -0500

    adds title section

---
+++ src/components/LiturgicCalendar/TitleSection.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import Title from "../../shared/Title";
+import SimpleText from "../../shared/SimpleText";
+import HorizontalLine from "../../shared/HorizontalLine";
+
+const MainContainer = styled.div`
+  height: 200px;
+  width: 100%;
+  display: flex;
+  justifcontent: center;
+  alignitems: center;
+  flex-direction: column;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  margin-top: 50px;
+`;
+
+function TitleSection() {
+  return (
+    <MainContainer>
+      <TitleContainer>
+        <HorizontalLine color="black" width={140} marRight={40} />
+        <Title variant="h4" space={5}>
+          CRONOGRAMA LITURGICO
+        </Title>
+        <HorizontalLine color="black" width={140} marLeft={40} />
+      </TitleContainer>
+      <SimpleText style={{ fontSize: 60, width: "100%", textAlign: "center" }}>
+        2021
+      </SimpleText>
+    </MainContainer>
+  );
+}
+
+export default TitleSection;
