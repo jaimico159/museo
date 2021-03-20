b853620ce69f2b2a296159c63fc36d9fa200f8e9
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-03-19 20:39:31 -0500

    adds grid calendar

--- src/components/LiturgicCalendar/subcomponents/Calendar.js
+++ src/components/LiturgicCalendar/subcomponents/Calendar.js
@@ -1,23 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import styled from "styled-components";
-import Title from "../../../shared/Title";
-import { Fragment } from "react";
-import SimpleText from "../../../shared/SimpleText";
 import ScheduleCard from "./ScheduleCard";
 
 const CalendarContainer = styled.div`
@@ -25,8 +6,8 @@ const CalendarContainer = styled.div`
   grid-template-columns: 230px 230px 230px 230px 230px 230px;
   grid-template-rows: 310px 310px;
   justify-content: center;
-  align-content: center;
-  height: 800px;
+  align-content: start;
+  height: 700px;
   width: 100%;
 `;
 
