c0696c98c1db527e10580be8de49984fad59009b
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2021-04-22 21:18:55 -0500

    adds link to services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -38,51 +38,61 @@ const servicesContent = [
     srcImage: "/images/services/victoria-picanteria.png",
     srcLogo: "/images/services/victoria-picanteria-logo.png",
     phone: "966714119",
+    link: "https://www.victoriaarequipa.com/home",
   },
   {
     srcImage: "/images/services/McDonalds.png",
     srcLogo: "/images/services/McDonalds-logo.png",
     phone: "(054) 212774",
+    link: "https://www.mcdonalds.com.pe/",
   },
   {
     srcImage: "/images/services/starbucks.png",
     srcLogo: "/images/services/starbucks-logo.png",
     phone: "(01) 5055000",
+    link: "https://www.starbucks.com.pe/",
   },
   {
     srcImage: "/images/services/pizza-hut.jpg",
     srcLogo: "/images/services/pizza-hut-logo.png",
     phone: "(054) 257777",
+    link: "https://www.pizzahut.com.pe/",
   },
   {
     srcImage: "/images/services/policia-de-turismo.jpg",
     srcLogo: "/images/services/policia-de-turismo-logo.png",
     phone: "988888888",
+    link: "https://www.policia.gob.pe/",
   },
   {
     srcImage: "/images/services/interbank.jpg",
     srcLogo: "/images/services/interbank-logo.png",
     phone: "(01) 3119000",
+    link: "https://interbank.pe/",
   },
   {
     srcImage: "/images/services/bcp.jpg",
     srcLogo: "/images/services/bcp-logo.png",
     phone: "(01) 3119000",
+    link: "https://www.viabcp.com/",
   },
   {
     srcImage: "/images/services/inkafarma.jpg",
     srcLogo: "/images/services/inkafarma-logo.png",
     phone: "988888888",
+    link: "https://inkafarma.pe/",
   },
   {
     srcImage: "/images/services/megatur.png",
     srcLogo: "/images/services/megatur-logo.png",
     phone: "(054) 404040",
+    link: "https://megatur.pe/",
   },
   {
     srcImage: "/images/services/lavanderia.jpg",
     srcLogo: "/images/services/lavanderia-logo.png",
     phone: "988888888",
+    link: "http://lavanderias-magic.com/",
   },
 ];
 
@@ -95,12 +105,14 @@ function Services() {
         <Slider {...settings} style={{ padding: "0 20px" }}>
           {servicesContent.map((el, index) => {
             return (
-              <ServiceCard
-                srcImage={el.srcImage}
-                key={index}
-                srcLogo={el.srcLogo}
-                phone={el.phone}
-              />
+              <a href={el.link} target="_blank">
+                <ServiceCard
+                  srcImage={el.srcImage}
+                  key={index}
+                  srcLogo={el.srcLogo}
+                  phone={el.phone}
+                />
+              </a>
             );
           })}
         </Slider>
